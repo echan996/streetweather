9d7098fcf8ad9d3a7648e4038570cdd0c79f1c62
echan996 <echan96@g.ucla.edu>
2017-09-13 21:18:20 -0700

    edit db_tables

--- venv/controller/database/database_tables.py
+++ venv/controller/database/database_tables.py
@@ -15,25 +15,25 @@ class Weather(Base):
     humidity = Column(Integer, nullable=False)
     wind = Column(Integer, nullable=False)
     date = Column(Date, nullable=False)"""
-    location_id = Column(Integer, ForeignKey('location.id'))
+    location_id = Column(Integer, nullable=False, ForeignKey('location.id'))
 
 
 class Clothes(Base):
     __tablename__ = 'clothes'
     id = Column(Integer, primary_key=True, autoincrement=True)
+	clothing_name = Column(String(30), nullable=True)
     """clothing_type_id = Column(Integer, nullable=False)
     color = Column(Integer, nullable=False)
     brand = Column(Integer, nullable=False)
-    clothing_name = Column(Integer, nullable=True)
     picture = Column(String(8), nullable=False)"""
 
 class User(Base):
 	__tablename__ = 'user'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(Integer, nullable=False)
+    username = Column(String(20), nullable=False)
     password = Column(String(20), nullable=False)
     gender = Column(Bit(2), nullable=False)
-    location_id = Column(Integer, ForeignKey('location.id'))
+    location_id = Column(Integer, ForeignKey('location.id'), nullable=False)
 
 class Location(Base):
 	__tablename__ = 'location'
@@ -43,24 +43,19 @@ class Location(Base):
 class User_Clothes_Joiner(Base):
 	__tablename__ = 'user_clothes_joiner'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    clothing_id = Column(Integer, ForeignKey('clothing.id'))
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    clothing_id = Column(Integer, ForeignKey('clothing.id'), nullable=False)
 
 class History(Base):
 	__tablename__ = 'history'
 	id = Column(Integer, primary_key=True, autoincrement=True)
-	user_id = Column(Integer, ForeignKey(user.id)) #i feel like this is off; i already have a user_id in the class above
-	preference = Column(Boolean, nullable=True)
-
-	"""docstring for User"""
-	def __init__(self, arg):
-		super(User, self).__init__()
-		self.arg = arg
-
+	user_id = Column(Integer, ForeignKey(user.id), nullable=False)
+	preference = Column(Boolean, nullable=False)
+	# some way of representing the clothing set. idk if sqlite supports lists.
 
 # Create an engine that stores data in the local directory's
 # sqlalchemy_example.db file.
-engine = create_engine('sqlite:///sqlalchemy_example.db')
+engine = create_engine('sqlite:///database.db')
 
 # Create all tables in the engine. This is equivalent to "Create Table"
 # statements in raw SQL.
