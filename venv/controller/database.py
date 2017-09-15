ae16c58cb8fb289e554db1e7c2b267d8e190f133
Andy Han <31876084+andyhan95@users.noreply.github.com>
2017-09-14 22:23:17 -0700

    deleted a test entry

--- venv/controller/database.py
+++ venv/controller/database.py
@@ -6,16 +6,21 @@ from sqlalchemy.orm import sessionmaker, create_session
 Session  = sessionmaker(bind = db.engine, autoflush=False)
 session = Session()
 weather_key = "b1b15e88fa797225412429c1c50c122a1"
-update_weather()
 print len(session.query(db.Location).all())
 
 def sample_get_location():
 	return session.query(db.Location).first().location_name
-	
+
+
+session.query(db.Location).filter(db.Location.location_name=='Londelbomble').delete()
+session.commit()
+
 def update_weather():
 	tuple_query = session.query(db.Location).all()
 	for tuple in tuple_query:
 		kv_params = {'appid': weather_key, 'q': tuple.location_name}
-		r = request.get("http://api.openweathermap.org/data/2.5/weather", params=kv_params)
+		r = requests.get("http://api.openweathermap.org/data/2.5/weather", params=kv_params)
 		print r.url
 		print r.text
+
+update_weather()
